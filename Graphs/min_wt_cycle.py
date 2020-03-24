0c05f4f828b5bc042bde7e0e40ed6ba187fb998c
SONU RAJ <srajsonu02@gmail.com>
2020-03-24 15:48:44 +0530

    Codebase updated

--- Graphs/min_wt_cycle.py
+++ Graphs/min_wt_cycle.py
@@ -1,10 +1,8 @@
 from _collections import defaultdict
 import heapq
 class Graph:
-    def __init__(self,m):
+    def __init__(self):
         self.graph=defaultdict(list)
-        self.vis=[False for _ in range(m)]
-        self.dis=[float('inf') for _ in range(m)]
 
     def add_edge(self,u,v,w):
         self.graph[u].append((v,w))
@@ -14,42 +12,36 @@ class Graph:
         self.graph[u].remove((v,w))
         self.graph[v].remove((u,w))
 
-    def shortest_path(self,u,v):
+    def shortest_dis(self,A,B,u,v):
+        dis=[float('inf')]*A
+        vis=[False]*A
         pq=[]
         heapq.heappush(pq,(u,0))
-        self.dis[u]=0
+        dis[u]=0
         while pq:
-            node,distance=heapq.heappop(pq)
-            self.vis[node]=True
+            node,distance = heapq.heappop(pq)
+            vis[node]=True
             for j,k in self.graph[node]:
-                new_dis=distance+k
-                if new_dis < self.dis[j]:
-                    self.dis[j]=new_dis
+                new_dis = distance + k
+                if new_dis < dis[j]:
+                    dis[j] = new_dis
                     heapq.heappush(pq,(j,new_dis))
-        return self.dis[v]
+        return dis[v]
 
-    def find_min(self,B):
-        min_cycle = float('inf')
-        for i,j in enumerate(self.graph):
-            for k in self.graph[j]:
-                l,m = k
-                print(l,m)
-                self.remove_edge(j,l,m)
-                dist = self.shortest_path(j,l)
-                min_cycle = min(min_cycle,dist+m)
-        # for i,j,k in B:
-        #     self.remove_edge(i-1,j-1,k)
-        #     dist = self.shortest_path(i-1,j-1)
-        #     min_cycle = min(min_cycle,dist+k)
-        #     self.add_edge(i-1,j-1,k)
+    def find_min_cycle(self,A,B):
+        min_cycle=float('inf')
+        for i,j,k in B:
+            self.remove_edge(i-1,j-1,k)
+            dist = self.shortest_dis(A,B,i-1,j-1)
+            min_cycle = min(min_cycle,dist + k)
         return min_cycle
 
 class Solution:
     def Solve(self,A,B):
-        graph = Graph(A)
+        graph = Graph()
         for i,j,k in B:
             graph.add_edge(i-1,j-1,k)
-        return graph.find_min(B)
+        return graph.find_min_cycle(A,B)
 
 A = 4
 B = [[1 ,2 ,2],
