052b97d8d9dac92bec868a774fd177903123b59b
SONU RAJ <srajsonu02@gmail.com>
2020-03-25 15:44:46 +0530

    Code base updated

---
+++ Graphs/first_depth_first_search_dfs.py
@@ -0,0 +1,25 @@
+from _collections import defaultdict
+class Solution:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def dfs(self,v,vis):
+        vis[v]=1
+        for i in self.graph[v]:
+            if not vis[i]:
+                self.dfs(i,vis)
+
+    def Solve(self,A,B,C):
+        n=len(A)
+        for i in range(n):
+            self.graph[A[i]].append(i+1)
+        vis=[0]*(n+1)
+        self.dfs(C,vis)
+        return vis[B]
+
+A=[1,1,1,3,3,2,2,7,6]
+B=9
+C=1
+
+D=Solution()
+print(D.Solve(A,B,C))
