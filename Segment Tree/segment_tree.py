ebc09ee94496b045219bd073a140402fffe30748
SONU RAJ <srajsonu02@gmail.com>
2020-04-15 01:35:27 +0530

    New code added

---
+++ Segment Tree/segment_tree.py
@@ -0,0 +1,44 @@
+class Solution:
+    def build(self,A,B,s,e,node):
+        if s==e:
+            B[node]=A[s]
+            return
+        mid = (s+e)//2
+        self.build(A,B,s,mid,2*node+1)
+        self.build(A,B,mid+1,e,2*node+2)
+        B[node]=B[2*node+1]+B[2*node+2]
+
+    def update(self,A,B,s,e,node,i,val):
+        if s==e:
+            A[i]=val
+            B[node]=val
+            return
+
+        mid=(s+e)//2
+        if i > mid:
+            self.update(A,B,mid+1,e,2*node+2,i,val)
+        else:
+            self.update(A,B,s,mid,2*node+1,i,val)
+        B[node] = B[2*node+1]+B[2*node+2]
+
+    def query(self,A,B,s,e,node,l,r):
+        if s > r or e < l:
+            return 0
+        if s >= l and e <= r:
+            return B[node]
+        mid = (s+e)//2
+        ans1 = self.query(A,B,s,mid,2*node+1,l,r)
+        ans2 = self.query(A,B,mid+1,e,2*node+2,l,r)
+        return ans1+ans2
+
+    def Solve(self,A):
+        n=len(A)
+        B=[0]*(4*n)
+        self.build(A,B,0,n-1,0)
+        self.update(A,B,0,n-1,0,0,3)
+        self.query(A,B,0,n-1,0,1,1)
+        return B
+
+A=[9,-8]
+B=Solution()
+print(B.Solve(A))
